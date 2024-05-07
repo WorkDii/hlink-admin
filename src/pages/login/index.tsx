166cc15b18ba2a1d816ca51b3544b76df3107b21
Usman Kareemee <usman2535@gmail.com>
2024-05-07 23:04:02 +0700

    remove register and forgot password pages

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,14 @@
 import { AuthPage } from "@refinedev/antd";
+import { TitleApp } from "../../components";
 
 export const Login = () => {
   return (
     <AuthPage
       type="login"
-      formProps={{
-        initialValues: { email: "demo@refine.dev", password: "demodemo" },
-      }}
+      formProps={{}}
+      title={<TitleApp collapsed={false}></TitleApp>}
+      registerLink={<div></div>}
+      forgotPasswordLink={<div></div>}
     />
   );
 };
