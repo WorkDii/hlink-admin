23363e67be8d34a94c12c45b6e690ded501d3b89
Usman Kareemee <usman2535@gmail.com>
2024-05-13 09:00:31 +0700

    show drug

--- src/pages/inventory_bill/list.tsx
+++ src/pages/inventory_bill/list.tsx
@@ -1,29 +1,45 @@
 import { List, useTable } from "@refinedev/antd";
 import { useResource, useUserFriendlyName } from "@refinedev/core";
-import { Table, Typography } from "antd";
+import { Table, Tag, Typography } from "antd";
 import BillStatusTag from "../bill_staus";
 const { Text } = Typography;
 import dayjs from "dayjs";
 
 export interface Ou {
-  id: string;
-  user_created: string;
-  date_created: Date;
-  user_updated: null;
-  date_updated: null;
   name: string;
 }
+export interface InventoryDrug {
+  quantity: number;
+  hospital_drug: HospitalDrug;
+}
+
+export interface HospitalDrug {
+  name: string;
+  default_unit: {
+    name: string;
+    name_eng: string;
+  };
+}
+
 export const InventoryBillList = () => {
   const { tableProps } = useTable({
     syncWithLocation: true,
     meta: {
-      fields: ["*", "status.*", "hcode.*", "pcucode.*"],
+      fields: [
+        "*",
+        "status.*",
+        "hcode.*",
+        "pcucode.name",
+        "inventory_drug.quantity",
+        "inventory_drug.hospital_drug.name",
+        "inventory_drug.hospital_drug.default_unit.*",
+      ],
     },
     sorters: { initial: [{ field: "date_created", order: "desc" }] },
   });
   const { resource } = useResource();
   const getUserFriendlyName = useUserFriendlyName();
-  console.log(tableProps);
+  console.log(tableProps.dataSource);
   return (
     <List
       headerProps={{
@@ -47,6 +63,22 @@ export const InventoryBillList = () => {
           sorter
           render={(v: { name: string; id: any }) => {
             return <BillStatusTag {...v}></BillStatusTag>;
+          }}
+        />
+        <Table.Column
+          dataIndex={"inventory_drug"}
+          title={"ยา"}
+          sorter
+          render={(v: InventoryDrug[]) => {
+            if (v.length === 0) return "-";
+            return v.map((hdrug) => {
+              return (
+                <Tag>
+                  {hdrug.hospital_drug.name} {hdrug.quantity}{" "}
+                  {hdrug.hospital_drug.default_unit?.name}
+                </Tag>
+              );
+            });
           }}
         />
         <Table.Column
