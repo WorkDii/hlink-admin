a2bafd2fe12f128a3e731cbebe887bd76b2afcd3
Usman Kareemee <usman2535@gmail.com>
2024-06-12 10:05:38 +0700

    fix prepack => 500 (เม็ด)

--- src/pages/inventory_request/create/requestDrugTable/unit_column.tsx
+++ src/pages/inventory_request/create/requestDrugTable/unit_column.tsx
@@ -38,7 +38,7 @@ export default function UnitColumn({ index, form }: Props) {
           </Select.Option>
         )}
         <Select.Option value={PREPACK_UNIT_ID}>
-          PREPACK / {prepack}
+          {prepack} ({defaultUnit.name})
         </Select.Option>
       </Select>
     </Form.Item>
