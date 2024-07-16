14ad54199a20c16aedde3ce9cefc68c7285f6f52
Usman Kareemee <usman2535@gmail.com>
2024-07-16 16:38:45 +0700

    add search to HospitalDrugList

--- src/pages/hospital_drug/list.tsx
+++ src/pages/hospital_drug/list.tsx
@@ -1,5 +1,7 @@
+import { SearchOutlined } from "@ant-design/icons";
 import { List, useTable, BooleanField } from "@refinedev/antd";
-import { Table, Typography } from "antd";
+import { Form, Grid, Input, Space, Table, Typography } from "antd";
+import { debounce } from "lodash";
 
 const { Text } = Typography;
 
@@ -8,16 +10,56 @@ const TrueIcon = () => <span>✅</span>;
 const FalseIcon = () => <span>❌</span>;
 
 export const HospitalDrugList = () => {
-  const { tableProps } = useTable({
+  const { tableProps, setFilters, searchFormProps } = useTable({
     syncWithLocation: true,
     meta: {
       fields: ["*", "default_unit.*", "hcode.name"],
     },
   });
+
+  const onSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      setFilters([], "replace");
+    } else {
+      setFilters(
+        [
+          {
+            field: "search",
+            operator: "eq",
+            value: e.target.value,
+          },
+        ],
+        "replace"
+      );
+    }
+  };
+  const debouncedOnChange = debounce(onSearch, 500);
+  const screens = Grid.useBreakpoint();
+
   return (
     <List
       headerProps={{
         subTitle: `รายกการยาของโรงพยาบาลแม่ข่าย`,
+      }}
+      headerButtons={() => {
+        return (
+          <Space
+            style={{
+              marginTop: screens.xs ? "1.6rem" : undefined,
+            }}
+          >
+            <Form {...searchFormProps} layout="inline">
+              <Form.Item name="name" noStyle>
+                <Input
+                  size="large"
+                  prefix={<SearchOutlined className="anticon tertiary" />}
+                  placeholder="Search by ชื่อยา รหัสยา"
+                  onChange={debouncedOnChange}
+                />
+              </Form.Item>
+            </Form>
+          </Space>
+        );
       }}
     >
       <Text type="danger">
