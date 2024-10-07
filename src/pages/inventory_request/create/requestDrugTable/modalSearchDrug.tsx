33d6b736b9a6c64fe8d620c251687d52e7e65360
Usman Kareemee <usman2535@gmail.com>
2024-10-07 11:01:44 +0700

    fix bug when hospitaldrug is_active = false

--- src/pages/inventory_request/create/requestDrugTable/modalSearchDrug.tsx
+++ src/pages/inventory_request/create/requestDrugTable/modalSearchDrug.tsx
@@ -26,6 +26,7 @@ export default function ModalSearchDrug({
     id: string;
     name: string;
     drugcode24: string;
+    is_active: boolean;
   }>({
     resource: "hospital_drug",
     meta: {
@@ -89,7 +90,7 @@ export default function ModalSearchDrug({
                 {item.name}
               </div>
               <Button
-                disabled={hospital_drug_selected.includes(item.id)}
+                disabled={hospital_drug_selected.includes(item.id) || !item.is_active}
                 onClick={async () => {
                   setIsAdding(true);
                   try {
@@ -103,7 +104,7 @@ export default function ModalSearchDrug({
                   }
                 }}
               >
-                เพิ่มรายการ
+                เพิ่มรายการ {item.is_active ? "" : "(ยกเลิกการใ)"}
               </Button>
             </List.Item>
           )}
